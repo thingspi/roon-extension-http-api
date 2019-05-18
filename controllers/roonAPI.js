a18a1669f870639c931a4ddd807fa01618744b8c
Herve Marcellini <herve_marcellini@fastmail.fm>
2019-05-17 20:42:23 -0700

    Format code

--- controllers/roonAPI.js
+++ controllers/roonAPI.js
@@ -1,261 +1,291 @@
-var RoonApi          = require("node-roon-api");
+var RoonApi = require("node-roon-api");
 var RoonApiTransport = require("node-roon-api-transport");
-var RoonApiStatus    = require("node-roon-api-status");
-var RoonApiImage     = require("node-roon-api-image");
-var RoonApiBrowse    = require("node-roon-api-browse");
+var RoonApiStatus = require("node-roon-api-status");
+var RoonApiImage = require("node-roon-api-image");
+var RoonApiBrowse = require("node-roon-api-browse");
 
-var path = require('path');
-
+var path = require("path");
 
 var core;
 var timeout;
 
 var roon = new RoonApi({
-   extension_id     : "st0g1e.roon-http-api",
-   display_name     : "roon-http-api",
-   display_version  : "1.0.2",
-   publisher        : "bastian ramelan",
-   email            :	"st0g1e@yahoo.com",
-   log_level        : "none",
+  extension_id: "st0g1e.roon-http-api",
+  display_name: "roon-http-api",
+  display_version: "1.0.2",
+  publisher: "bastian ramelan",
+  email: "st0g1e@yahoo.com",
+  log_level: "none",
 
-   core_paired: function(core_) {
-     core = core_;
-   	 core.services.RoonApiTransport.subscribe_zones((response, msg) => {
-     });
-   },
+  core_paired: function(core_) {
+    core = core_;
+    core.services.RoonApiTransport.subscribe_zones((response, msg) => {});
+  },
 
-   core_unpaired: function(core_) {
-
-   }
+  core_unpaired: function(core_) {}
 });
 
 var svc_status = new RoonApiStatus(roon);
 
 roon.init_services({
-   required_services: [ RoonApiTransport, RoonApiBrowse, RoonApiImage ],
-   provided_services: [ svc_status ],
+  required_services: [RoonApiTransport, RoonApiBrowse, RoonApiImage],
+  provided_services: [svc_status]
 });
 
 svc_status.set_status("Extension enabled", false);
 roon.start_discovery();
 
-
 // --------------- APIs ------------------
 
-exports.getCore = function(req, res){
+exports.getCore = function(req, res) {
   res.send({
-    "id": core.core_id,
-    "display_name": core.display_name,
-    "display_version": core.display_version
+    id: core.core_id,
+    display_name: core.display_name,
+    display_version: core.display_version
   });
 };
 
 exports.listZones = function(req, res) {
-    core.services.RoonApiTransport.get_zones((iserror, body) => {
-        if (!iserror) {
-            res.send({
-                "zones": body.zones
-            })
-        }
-    })
+  core.services.RoonApiTransport.get_zones((iserror, body) => {
+    if (!iserror) {
+      res.send({
+        zones: body.zones
+      });
+    }
+  });
 };
 
 exports.listOutputs = function(req, res) {
-    core.services.RoonApiTransport.get_outputs((iserror, body) => {
-        if (!iserror) {
-            res.send({
-                "outputs": body.outputs
-            })
-        }
-    })
+  core.services.RoonApiTransport.get_outputs((iserror, body) => {
+    if (!iserror) {
+      res.send({
+        outputs: body.outputs
+      });
+    }
+  });
 };
 exports.getZone = function(req, res) {
   res.send({
-    "zone": core.services.RoonApiTransport.zone_by_zone_id(req.query['zoneId'])
-  })
+    zone: core.services.RoonApiTransport.zone_by_zone_id(req.query["zoneId"])
+  });
 };
 
 exports.play_pause = function(req, res) {
-    core.services.RoonApiTransport.control(req.query['zoneId'], 'playpause');
+  core.services.RoonApiTransport.control(req.query["zoneId"], "playpause");
 
-   res.send({
-    "status": "success"
-  })
+  res.send({
+    status: "success"
+  });
 };
 
 exports.stop = function(req, res) {
-    core.services.RoonApiTransport.control(req.query['zoneId'], 'stop');
+  core.services.RoonApiTransport.control(req.query["zoneId"], "stop");
 
-   res.send({
-    "status": "success"
-  })
+  res.send({
+    status: "success"
+  });
 };
 
 exports.play = function(req, res) {
-    core.services.RoonApiTransport.control(req.query['zoneId'], 'play');
+  core.services.RoonApiTransport.control(req.query["zoneId"], "play");
 
-   res.send({
-    "zone": "Success"
-  })
+  res.send({
+    zone: "Success"
+  });
 };
 
 exports.pause = function(req, res) {
-    core.services.RoonApiTransport.control(req.query['zoneId'], 'pause');
+  core.services.RoonApiTransport.control(req.query["zoneId"], "pause");
 
-   res.send({
-    "status": "success"
-  })
+  res.send({
+    status: "success"
+  });
 };
 
-
 exports.previous = function(req, res) {
-    core.services.RoonApiTransport.control(req.query['zoneId'], 'previous');
+  core.services.RoonApiTransport.control(req.query["zoneId"], "previous");
 
-//    setTimeout(function(){
-       res.send({
-         "zone": req.headers.referer
-       })
-//    }, 2000);
+  //    setTimeout(function(){
+  res.send({
+    zone: req.headers.referer
+  });
+  //    }, 2000);
 };
 
 exports.next = function(req, res) {
-  core.services.RoonApiTransport.control(req.query['zoneId'], 'next');
+  core.services.RoonApiTransport.control(req.query["zoneId"], "next");
 
-//    setTimeout(function(){
-       res.send({
-         "zone": core.services.RoonApiTransport.zone_by_zone_id(req.query['zoneId'])
-       })
-//    }, 2000);
+  //    setTimeout(function(){
+  res.send({
+    zone: core.services.RoonApiTransport.zone_by_zone_id(req.query["zoneId"])
+  });
+  //    }, 2000);
 };
 
 exports.change_volume = function(req, res) {
-  core.services.RoonApiTransport.change_volume(req.query['outputId'], "absolute", req.query['volume']);
-
+  core.services.RoonApiTransport.change_volume(
+    req.query["outputId"],
+    "absolute",
+    req.query["volume"]
+  );
 
   res.send({
-    "status": "success"
-  })
+    status: "success"
+  });
 };
 
-exports.getMediumImage = function( req, res ) {
-  get_image( req.query['image_key'], "fit", 300, 200, "image/jpeg", res);
+exports.getMediumImage = function(req, res) {
+  get_image(req.query["image_key"], "fit", 300, 200, "image/jpeg", res);
 };
 
-exports.getIcon = function( req, res ) {
-  get_image( req.query['image_key'], "fit", 100, 100, "image/jpeg", res);
+exports.getIcon = function(req, res) {
+  get_image(req.query["image_key"], "fit", 100, 100, "image/jpeg", res);
 };
 
 exports.getImage = function(req, res) {
-   get_image( req.query['image_key'], "fit", 300, 200, "image/jpeg", res);
+  get_image(req.query["image_key"], "fit", 300, 200, "image/jpeg", res);
 };
 
 exports.getOriginalImage = function(req, res) {
-  core.services.RoonApiImage.get_image(req.query['image_key'], function(cb, contentType, body) {
+  core.services.RoonApiImage.get_image(req.query["image_key"], function(
+    cb,
+    contentType,
+    body
+  ) {
+    res.contentType = contentType;
 
-     res.contentType = contentType;
-
-     res.writeHead(200, {'Content-Type': 'image/jpeg' });
-     res.end(body, 'binary');
+    res.writeHead(200, { "Content-Type": "image/jpeg" });
+    res.end(body, "binary");
   });
 };
 
 function get_image(image_key, scale, width, height, format, res) {
-   core.services.RoonApiImage.get_image(image_key, {scale, width, height, format}, function(cb, contentType, body) {
-
+  core.services.RoonApiImage.get_image(
+    image_key,
+    { scale, width, height, format },
+    function(cb, contentType, body) {
       res.contentType = contentType;
 
-      res.writeHead(200, {'Content-Type': 'image/jpeg' });
-      res.end(body, 'binary');
-   });
-};
+      res.writeHead(200, { "Content-Type": "image/jpeg" });
+      res.end(body, "binary");
+    }
+  );
+}
 
 exports.listByItemKey = function(req, res) {
-   refresh_browse( req.query['zoneId'], { item_key: req.query['item_key'] }, req.query['page'], req.query['list_size'], function(myList) {
-
-   res.send({
-     "list": myList
-   })
-  });
+  refresh_browse(
+    req.query["zoneId"],
+    { item_key: req.query["item_key"] },
+    req.query["page"],
+    req.query["list_size"],
+    function(myList) {
+      res.send({
+        list: myList
+      });
+    }
+  );
 };
 
 exports.listSearch = function(req, res) {
-   refresh_browse( req.query['zoneId'], { item_key: req.query['item_key'], input: req.query['toSearch'] }, req.query['page'], req.query['list_size'], function(myList) {
-    res.send({
-      "list": myList
-    })
-  });
+  refresh_browse(
+    req.query["zoneId"],
+    { item_key: req.query["item_key"], input: req.query["toSearch"] },
+    req.query["page"],
+    req.query["list_size"],
+    function(myList) {
+      res.send({
+        list: myList
+      });
+    }
+  );
 };
 
 exports.goUp = function(req, res) {
-   refresh_browse( req.query['zoneId'], { pop_levels: 1 }, 1, req.query['list_size'],  function(myList) {
-
-    res.send({
-      "list": myList
-    })
-  });
-
+  refresh_browse(
+    req.query["zoneId"],
+    { pop_levels: 1 },
+    1,
+    req.query["list_size"],
+    function(myList) {
+      res.send({
+        list: myList
+      });
+    }
+  );
 };
 
 exports.goHome = function(req, res) {
-   refresh_browse( req.query['zoneId'], { pop_all: true }, 1, req.query['list_size'], function(myList) {
-
-   res.send({
-     "list": myList
-    })
-  });
+  refresh_browse(
+    req.query["zoneId"],
+    { pop_all: true },
+    1,
+    req.query["list_size"],
+    function(myList) {
+      res.send({
+        list: myList
+      });
+    }
+  );
 };
 
 exports.listGoPage = function(req, res) {
-   load_browse( req.query['page'], req.query['list_size'], function(myList) {
-
-   res.send({
-     "list": myList
-    })
+  load_browse(req.query["page"], req.query["list_size"], function(myList) {
+    res.send({
+      list: myList
+    });
   });
-
 };
 
 exports.listRefresh = function(req, res) {
-   refresh_browse( req.query['zoneId'], { refresh_list: true }, 0, 0, function(myList) {
-
-   res.send({
-     "list": myList
-    })
+  refresh_browse(req.query["zoneId"], { refresh_list: true }, 0, 0, function(
+    myList
+  ) {
+    res.send({
+      list: myList
+    });
   });
 };
-
 
 // Timers
 
 exports.addTimer = function(req, res) {
-  save_timer(req.query['zoneId'], req.query['time'], req.query['command'], req.query['isRepeat']);
+  save_timer(
+    req.query["zoneId"],
+    req.query["time"],
+    req.query["command"],
+    req.query["isRepeat"]
+  );
 
   run_later();
   var timers = get_timers();
 
   res.send({
-    "timers": timers
-  })
+    timers: timers
+  });
 };
 
 exports.getTimers = function(req, res) {
   var timers = get_timers();
 
   res.send({
-    "timers": timers
-  })
+    timers: timers
+  });
 };
 
 exports.removeTimer = function(req, res) {
   var timers = get_timers();
-  var zoneToRemove = req.query['zoneId'];
-  var timeToRemove = req.query['time'];
-  var commandToRemove = req.query['command'];
-  var isRepeatToRemove = req.query['isRepeat'];
+  var zoneToRemove = req.query["zoneId"];
+  var timeToRemove = req.query["time"];
+  var commandToRemove = req.query["command"];
+  var isRepeatToRemove = req.query["isRepeat"];
 
-  for ( var i in timers ) {
-    if ( timers[i].zoneId == zoneToRemove && timers[i].time == timeToRemove &&
-         timers[i].command == commandToRemove && timers[i].isRepeat == isRepeatToRemove ) {
+  for (var i in timers) {
+    if (
+      timers[i].zoneId == zoneToRemove &&
+      timers[i].time == timeToRemove &&
+      timers[i].command == commandToRemove &&
+      timers[i].isRepeat == isRepeatToRemove
+    ) {
       timers.splice(i, 1);
       break;
     }
@@ -267,55 +297,65 @@ exports.removeTimer = function(req, res) {
   var timers = get_timers();
 
   res.send({
-   "timers": timers
-  })
+    timers: timers
+  });
 };
 
-
 function refresh_browse(zone_id, opts, page, listPerPage, cb) {
-    var items = [];
-    opts = Object.assign({
-        hierarchy:          "browse",
-        zone_or_output_id:  zone_id,
-    }, opts);
+  var items = [];
+  opts = Object.assign(
+    {
+      hierarchy: "browse",
+      zone_or_output_id: zone_id
+    },
+    opts
+  );
 
+  core.services.RoonApiBrowse.browse(opts, (err, r) => {
+    if (err) {
+      console.log(err, r);
+      return;
+    }
 
-    core.services.RoonApiBrowse.browse(opts, (err, r) => {
-        if (err) { console.log(err, r); return; }
+    if (r.action == "list") {
+      page = (page - 1) * listPerPage;
 
-        if (r.action == 'list') {
-            page = ( page - 1 ) * listPerPage;
+      core.services.RoonApiBrowse.load(
+        {
+          hierarchy: "browse",
+          offset: page,
+          set_display_offset: listPerPage
+        },
+        (err, r) => {
+          items = r.items;
 
-            core.services.RoonApiBrowse.load({
-                hierarchy:          "browse",
-                offset:             page,
-                set_display_offset: listPerPage,
-            }, (err, r) => {
-                items = r.items;
-
-                cb(r.items);
-            });
+          cb(r.items);
         }
-    });
+      );
+    }
+  });
 }
 
 function load_browse(page, listPerPage, cb) {
-   page = ( page - 1 ) * listPerPage;
+  page = (page - 1) * listPerPage;
 
-   core.services.RoonApiBrowse.load({
-      hierarchy:          "browse",
-      offset:             page,
-      set_display_offset: page,
-   }, (err, r) => {
+  core.services.RoonApiBrowse.load(
+    {
+      hierarchy: "browse",
+      offset: page,
+      set_display_offset: page
+    },
+    (err, r) => {
       cb(r.items);
-    });
+    }
+  );
 }
 
 function runCommand(command, zone_id) {
-  if ( command == "play" ) {
-    core.services.RoonApiTransport.control(req.query['zoneId'], 'play');
-  } else if ( command == "pause" ) {
-    core.services.RoonApiTransport.control(req.query['zoneId'], 'pause');
+  if (command == "play") {
+    core.services.RoonApiTransport.control(req.query["zoneId"], "play");
+  } else if (command == "pause") {
+    core.services.RoonApiTransport.control(req.query["zoneId"], "pause");
   }
 }
 
@@ -328,11 +368,11 @@ function get_timers() {
 function save_timer(zoneId, time, command, isRepeat) {
   var timers = get_timers();
 
-  if ( timers == null ) {
+  if (timers == null) {
     timers = [];
   }
 
-  var toAdd = {}
+  var toAdd = {};
   toAdd.zoneId = zoneId;
   toAdd.time = time;
   toAdd.command = command;
@@ -351,22 +391,24 @@ function refresh_timer() {
   var newTimers = [];
   var isFirst = true;
 
-  for ( var i in timers ) {
-     if ( timers[i].time >= dateNow.getTime() ) {
-         newTimers.push( timers[i] );
-     }
+  for (var i in timers) {
+    if (timers[i].time >= dateNow.getTime()) {
+      newTimers.push(timers[i]);
+    }
   }
   newTimers.sort(compare);
   roon.save_config("my_timers", newTimers);
-
 }
 
 function compare(a, b) {
-  if ( a.time < b.time ) { return -1; }
-  if ( a.time > b.time ) { return 1; }
+  if (a.time < b.time) {
+    return -1;
+  }
+  if (a.time > b.time) {
+    return 1;
+  }
   return 0;
 }
-
 
 function run_later() {
   clearTimeout(timeout);
@@ -374,7 +416,7 @@ function run_later() {
   var timers = get_timers();
   var timer;
 
-  if ( timers != null && timers.length > 0 ) {
+  if (timers != null && timers.length > 0) {
     timer = timers[0];
 
     var date = new Date(parseInt(timer.time));
@@ -382,13 +424,13 @@ function run_later() {
 
     var lapse = date - curDate;
 
-    if ( timer.command == "play" ) {
-      timeout = setTimeout( function () {
+    if (timer.command == "play") {
+      timeout = setTimeout(function() {
         playZone(timer.zoneId);
         run_later();
-      },  lapse);
-    } else if ( timer.command == "pause" ) {
-      timeout = setTimeout( function() {
+      }, lapse);
+    } else if (timer.command == "pause") {
+      timeout = setTimeout(function() {
         pauseZone(timer.zoneId);
         run_later();
       }, lapse);
@@ -398,10 +440,10 @@ function run_later() {
 
 function playZone(zoneId) {
   refresh_timer();
-  core.services.RoonApiTransport.control(zoneId, 'play');
+  core.services.RoonApiTransport.control(zoneId, "play");
 }
 
 function pauseZone(zoneId) {
   refresh_timer();
-  core.services.RoonApiTransport.control(zoneId, 'pause');
+  core.services.RoonApiTransport.control(zoneId, "pause");
 }
