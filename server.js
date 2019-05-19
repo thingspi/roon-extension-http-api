a32be12bf6e1a4170338023554f45899cad7ab7e
Herve Marcellini <herve_marcellini@fastmail.fm>
2019-05-18 20:59:05 -0700

    Reformatted Javascript

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-var express = require('express');
-var path = require('path');
+var express = require("express");
+var path = require("path");
 
 var app = express();
 
-const PORT=3001;
+const PORT = 3001;
 
-app.use(express.static(path.join(__dirname, 'htmls')));
+app.use(express.static(path.join(__dirname, "htmls")));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
-
-require('./routes')(app);
+require("./routes")(app);
 
 app.listen(PORT);
 
-console.log('Listening on port: ' + PORT);
+console.log("Listening on port: " + PORT);
